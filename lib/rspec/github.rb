28bd88c3b8437882fb274f40a719fb0646ac1b50
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2020-04-02 15:28:43 +0200

    Bundle gem generator

---
+++ lib/rspec/github.rb
@@ -0,0 +1,8 @@
+require "rspec/github/version"
+
+module Rspec
+  module Github
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/github/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module Github
+    VERSION = "0.1.0"
+  end
+end
